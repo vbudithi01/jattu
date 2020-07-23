ec84804af339df176b18645ee4d57ef42120aed3
vijayarao-techmojo <65339457+vijayarao-techmojo@users.noreply.github.com>
2020-07-23 07:13:50 +0530

    test commit to Master

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
   public static void main(String[] args) {
 	LocalTime currentTime = new LocalTime();
 	System.out.println("The current local time is: " + currentTime);
+	System.out.println("This is from Master);
 
 	Greeter greeter = new Greeter();
 	System.out.println(greeter.sayHello());
